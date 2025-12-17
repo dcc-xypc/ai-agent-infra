5a636873a92e619d5a49c9213370c5474d118478
daicongcong <dai.congcong@trans-cosmos.com.cn>
2025-12-17 14:12:17 +0800

    refactor(variables): 重构变量定义格式并添加新变量
    将单行变量定义改为多行格式以提高可读性，并新增多个数据库和Keycloak相关变量

--- modules/cloudrun/variables.tf
+++ modules/cloudrun/variables.tf
@@ -1,26 +1,69 @@
-variable "project_id" { type = string }
-variable "region" { type = string }
-variable "env_name" { type = string }
-variable "connector_id" { type = string }
-variable "external_cloudrun_sa_email" { type = string }
+variable "project_id" {
+  type = string
+}
+variable "region" {
+  type = string
+}
+variable "env_name" {
+  type = string
+}
+variable "connector_id" {
+  type = string
+}
+variable "external_cloudrun_sa_email" {
+  type = string
+}
+variable "default_placeholder_image" {
+  default = "gcr.io/cloudrun/container/hello"
+}
+variable "oauth2_proxy_image" {
+  default = "quay.io/oauth2-proxy/oauth2-proxy:v7.13.0-amd64"
+}
+variable "ai_agent_db_connection_name" {
+  type = string
+}
+variable "ai_agent_db_name" {
+  type = string
+}
+variable "ai_agent_db_user" {
+  type = string
+}
+variable "ai_agent_db_password" {
+  type = string
+  sensitive = true
+}
+variable "keycloak_db_connection_name" {
+  type = string
+}
+variable "keycloak_db_name" {
+  type = string
+}
+variable "keycloak_db_user" {
+  type = string
+}
+variable "keycloak_db_password" {
+  type = string
+  sensitive = true
+}
+variable "keycloak_admin_name" {
+  type = string
+}
+variable "keycloak_admin_password" {
+  type = string
+  sensitive = true
+}
+variable "keycloak_external_url" {
+  type = string
+}
 
-variable "default_placeholder_image" { default = "gcr.io/cloudrun/container/hello" }
-variable "oauth2_proxy_image" { default = "quay.io/oauth2-proxy/oauth2-proxy:v7.13.0-amd64"} 
-
-variable "ai_agent_db_connection_name" { type = string } 
-variable "ai_agent_db_name" { type = string }
-variable "ai_agent_db_user" { type = string }
-variable "ai_agent_db_password" { type = string, sensitive = true } 
-
-variable "keycloak_db_connection_name" { type = string }
-variable "keycloak_db_name" { type = string }
-variable "keycloak_db_user" { type = string }
-variable "keycloak_db_password" { type = string, sensitive = true }
-
-variable "keycloak_admin_name" { type = string }
-variable "keycloak_admin_password" { type = string, sensitive = true }
-variable "keycloak_external_url" { type = string }
-
-variable "oauth2_proxy_client_id" { type = string }
-variable "oauth2_proxy_client_secret" { type = string, sensitive = true }
-variable "oauth2_proxy_cookie_secret" { type = string, sensitive = true }
+variable "oauth2_proxy_client_id" {
+  type = string
+}
+variable "oauth2_proxy_client_secret" {
+  type = string
+  sensitive = true
+}
+variable "oauth2_proxy_cookie_secret" {
+  type = string
+  sensitive = true
+}
