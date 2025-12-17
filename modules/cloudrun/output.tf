b3d09f96b571fbd48938768b13f1df2357a6ec9c
daicongcong <dai.congcong@trans-cosmos.com.cn>
2025-12-17 14:02:54 +0800

    refactor(cloudrun): 重命名输出变量以区分不同服务
    将通用的服务名称和URL输出变量重命名为特定于前端、后端和Keycloak认证服务的变量，以提高代码可读性和明确性

--- modules/cloudrun/output.tf
+++ modules/cloudrun/output.tf
@@ -1,9 +1,29 @@
-output "service_name" {
+output "web_frontend_app_name" {
   description = "Cloud Runサービスの名前です。"
-  value       = google_cloud_run_v2_service.app_service.name
+  value       = google_cloud_run_v2_service.web_frontend_app.name
 }
 
-output "app_service_url" {
+output "web_frontend_app_url" {
   description = "Cloud Runサービスの公開URLです。"
-  value       = google_cloud_run_v2_service.app_service.uri
+  value       = google_cloud_run_v2_service.web_frontend_app.uri
+}
+
+output "web_backend_app_name" {
+  description = "Cloud Runサービスの名前です。"
+  value       = google_cloud_run_v2_service.web_backend_app.name
+}
+
+output "web_frontend_app_url" {
+  description = "Cloud Runサービスの公開URLです。"
+  value       = google_cloud_run_v2_service.web_backend_app.uri
+}
+
+output "auth_keycloak_app_name" {
+  description = "Cloud Runサービスの名前です。"
+  value       = google_cloud_run_v2_service.auth_keycloak_app.name
+}
+
+output "auth_keycloak_app_url" {
+  description = "Cloud Runサービスの公開URLです。"
+  value       = google_cloud_run_v2_service.auth_keycloak_app.uri
 }
