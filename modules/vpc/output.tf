029a9b52a7e4931d6992e61fa8eaac4dc8941441
daicongcong <dai.congcong@trans-cosmos.com.cn>
2025-12-22 17:27:56 +0800

    update

--- modules/vpc/output.tf
+++ modules/vpc/output.tf
@@ -1,3 +1,8 @@
+output "vpc_id" {
+  value       = google_compute_network.vpc_network.id
+  description = "VPC 网络的唯一标识符 (self_link)。"
+}
+
 output "network_self_link" {
   description = "作成されたVPCネットワークのセルフリンクです。"
   value       = google_compute_network.vpc_network.self_link
@@ -12,3 +17,4 @@ output "ops_subnet_id" {
   description = "运维管理专用子网 ID，用于部署维护机（Ops VM）。"
   value       = google_compute_subnetwork.ops_subnet.id
 }
+
