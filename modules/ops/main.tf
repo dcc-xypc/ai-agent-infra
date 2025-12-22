c88f007efff8e107073a644b57c1ceb6854ac3d0
daicongcong <dai.congcong@trans-cosmos.com.cn>
2025-12-22 18:20:16 +0800

    modify

--- modules/ops/main.tf
+++ modules/ops/main.tf
@@ -42,3 +42,45 @@ resource "google_compute_firewall" "allow_iap_ssh_ops" {
   source_ranges = ["35.235.240.0/20"]
   target_tags   = ["ops-admin"]
 }
+
+resource "null_resource" "install_psql" {
+  # 确保在 VM 和防火墙规则都创建好之后再执行
+  depends_on = [
+    google_compute_instance.ops_vm,
+    # 假设你的防火墙资源名是这个，请根据实际修改
+    google_compute_firewall.allow_iap_ssh 
+  ]
+
+  triggers = {
+    # 只有当 VM 的 ID 改变时才重新执行，或者你可以手动更改此值强制触发
+    instance_id = google_compute_instance.ops_vm.id
+  }
+
+  # 1. 下载安装包（在运行 Terraform 的本地环境执行，如 Cloud Shell）
+  provisioner "local-exec" {
+    command = "apt-get download postgresql-client-15 postgresql-client-common libpq5"
+  }
+
+  # 2. 使用 SCP 将包传上去
+  provisioner "local-exec" {
+    command = <<-EOT
+      gcloud compute scp postgresql-client*.deb libpq5*.deb ${google_compute_instance.ops_vm.name}:/tmp/ \
+        --tunnel-through-iap \
+        --project=${var.project_id} \
+        --zone=${google_compute_instance.ops_vm.zone} \
+        --quiet
+    EOT
+  }
+
+  # 3. 使用 SSH 执行安装命令
+  provisioner "local-exec" {
+    command = <<-EOT
+      gcloud compute ssh ${google_compute_instance.ops_vm.name} \
+        --tunnel-through-iap \
+        --project=${var.project_id} \
+        --zone=${google_compute_instance.ops_vm.zone} \
+        --quiet \
+        --command="sudo dpkg -i /tmp/*.deb || sudo apt-get install -f -y"
+    EOT
+  }
+}
