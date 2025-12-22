419823ddd4793dff29a1e070e3ab2298852eed7b
daicongcong <dai.congcong@trans-cosmos.com.cn>
2025-12-22 20:22:27 +0800

    modify

--- modules/ops/main.tf
+++ modules/ops/main.tf
@@ -33,32 +33,3 @@ resource "google_compute_firewall" "allow_iap_ssh_ops" {
   source_ranges = ["35.235.240.0/20"]
   target_tags   = ["ops-admin"]
 }
-
-resource "null_resource" "remote_install" {
-
-  triggers = {
-    # 如果 VM 重建了，或者 NAT 重新开启了，就触发安装
-    instance_id = google_compute_instance.ops_vm.id
-    nat_id      = var.nat_id
-  }
-
-  provisioner "local-exec" {
-    command = <<-EOT
-      if [ -z "${var.nat_id}" ] || [ "${var.nat_id}" = "null" ]; then
-        echo "NAT 未开启，跳过安装步骤。"
-        exit 0
-      fi
-      echo "检测到 NAT 已开启，准备安装工具..."
-
-      for i in {1..10}; do
-        gcloud compute ssh ${google_compute_instance.ops_vm.name} \
-          --tunnel-through-iap \
-          --project=${var.project_id} \
-          --zone=${google_compute_instance.ops_vm.zone} \
-          --quiet \
-          --command="sudo apt-get update && sudo apt-get install -y postgresql-client" && break || \
-          (echo "等待 VM 或网络就绪... ($i/10)" && sleep 10)
-      done
-    EOT
-  }
-}
