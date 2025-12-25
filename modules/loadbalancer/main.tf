8f7eda1f8b4b428bf8bc756c169763bdaaacb574
daicongcong <dai.congcong@trans-cosmos.com.cn>
2025-12-25 19:24:09 +0800

    modify

--- modules/loadbalancer/main.tf
+++ modules/loadbalancer/main.tf
@@ -200,6 +200,7 @@ resource "google_compute_region_backend_service" "internal_backend" {
   # 引用现有的 frontend_neg (业务 Cloud Run) 
   backend {
     group = google_compute_region_network_endpoint_group.frontend_neg.id
+    balancing_mode = null
   }
 }
 
