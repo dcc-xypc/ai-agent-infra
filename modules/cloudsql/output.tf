5e17a337415ffcc25d95f678a6a36f3635c10eeb
dcc-xypc <42265606+dcc-xypc@users.noreply.github.com>
2025-12-31 13:05:53 +0800

    Update output.tf

--- modules/cloudsql/output.tf
+++ modules/cloudsql/output.tf
@@ -3,7 +3,10 @@
 # ----------------------------------------------------
 # 1. Cloud SQL インスタンス接続名 (CLOUD_SQL_CONN_NAME)
 # ----------------------------------------------------
-
+output "instance_connection_name" {
+  value       = google_sql_database_instance.postgres_instance.connection_name
+  description = "Cloud SQLインスタンスの接続名で、Cloud Runによって使用されます"
+}
 
 output "mysql_instance_connection_name" {
   value       = google_sql_database_instance.mysql_instance.connection_name
@@ -26,3 +29,12 @@ output "ai_agent_user_name" {
 # ----------------------------------------------------
 # 3. Keycloak データベース接続情報 (auth-keycloak-app 用)
 # ----------------------------------------------------
+output "keycloak_db_name" {
+  value       = google_sql_database.keycloak_db.name
+  description = "Keycloak認証のデータベース名"
+}
+
+output "keycloak_user_name" {
+  value       = google_sql_user.keycloak_user.name
+  description = "Keycloakデータベース接続のユーザー名"
+}
